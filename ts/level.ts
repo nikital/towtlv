194b5f43a4fb941bfc86668c586adf9f382b969a
Nikita Leshenko <nikita@leshenko.net>
2015-01-23 13:18:49 +0200

    Create 4 wheels

--- ts/level.ts
+++ ts/level.ts
@@ -12,17 +12,17 @@ class Level
 
     private init_phys():void
     {
-        var gravity = new b2Vec2(0, 1);
+        var gravity = new b2Vec2(0, 0);
         this.world = new b2World(gravity, true);
 
-        this.tow_truck = new Tow_truck(this.world);
+        this.tow_truck = new Tow_truck(this.world, new b2Vec2(4, 4));
 
         this.world.SetDebugDraw(this.debug_draw);
     }
 
     public on_tick():void
     {
-        this.world.Step(1/60, 15, 15);
+        this.world.Step(1/60, 6, 3);
         this.world.DrawDebugData();
     }
 }
--- ts/tow_truck.ts
+++ ts/tow_truck.ts
@@ -1,22 +1,60 @@
 class Tow_truck
 {
-    constructor(private world:b2World)
+    private body:b2Body;
+
+    constructor(private world:b2World, position:b2Vec2)
     {
-        var ground_body_def = new b2BodyDef();
-        ground_body_def.position.Set(2, 2);
-        ground_body_def.type = b2Body.b2_dynamicBody;
-
-        var ground_fix_def = new b2FixtureDef();
-        var poly = new b2PolygonShape();
-        poly.SetAsBox(1.5, 3);
-        ground_fix_def.shape = poly;
-
-        var ground = this.world.CreateBody(ground_body_def);
-        ground.CreateFixture(ground_fix_def);
-
+        this.create_body(position);
+        this.create_wheels(position);
     }
 
-    public set_position(pos:b2Vec2):void
+    private create_body(position:b2Vec2):void
     {
+        var body_def = new b2BodyDef();
+        body_def.type = b2Body.b2_dynamicBody;
+        body_def.position = position;
+        // add linear and angular damping
+
+        var fix_def = new b2FixtureDef();
+        var poly = new b2PolygonShape();
+        poly.SetAsBox(1.5, 3);
+        fix_def.shape = poly;
+
+        this.body = this.world.CreateBody(body_def);
+        this.body.CreateFixture(fix_def);
+    }
+
+    private create_wheels(position:b2Vec2):void
+    {
+        var wheel_offset = new b2Vec2(1.2, 2.3);
+
+        var body_def = new b2BodyDef();
+        body_def.type = b2Body.b2_dynamicBody;
+
+        var fix_def = new b2FixtureDef();
+        var poly = new b2PolygonShape();
+        poly.SetAsBox(0.15, 0.4);
+        fix_def.shape = poly;
+
+        body_def.position = position.Copy();
+        body_def.position.Add(new b2Vec2(wheel_offset.x, -wheel_offset.y));
+        var right_front = this.world.CreateBody(body_def);
+        right_front.CreateFixture(fix_def);
+
+        body_def.position = position.Copy();
+        body_def.position.Add(new b2Vec2(-wheel_offset.x, -wheel_offset.y));
+        var left_front = this.world.CreateBody(body_def);
+        left_front.CreateFixture(fix_def);
+
+        body_def.position = position.Copy();
+        body_def.position.Add(new b2Vec2(wheel_offset.x, wheel_offset.y));
+        var right_rear = this.world.CreateBody(body_def);
+        right_rear.CreateFixture(fix_def);
+
+        body_def.position = position.Copy();
+        body_def.position.Add(new b2Vec2(-wheel_offset.x, wheel_offset.y));
+        var left_rear = this.world.CreateBody(body_def);
+        left_rear.CreateFixture(fix_def);
+
     }
 }
