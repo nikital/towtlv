2308d62f438b5eb6517a5422fe497b60d554d0e2
Nikita Leshenko <nikita@leshenko.net>
2015-01-24 15:33:06 +0200

    Props by Alon

--- js/levels.js
+++ js/levels.js
@@ -1,4 +1,4 @@
 var g_levels = {
-    0: {"background":"bg/plain","props":[{"prop":"tow","x":279,"y":97,"rotation":0},{"prop":"target","x":278,"y":296,"rotation":-177},{"prop":"wall","x":474,"y":91,"rotation":0},{"prop":"wall","x":606,"y":88,"rotation":0}]}
+    0: {"background":"bg/plain","props":[{"prop":"trash_can","x":329,"y":110,"rotation":0},{"prop":"suitcase","x":601,"y":200,"rotation":0},{"prop":"stroller","x":180,"y":170,"rotation":0},{"prop":"cafe_umbrella","x":387,"y":229,"rotation":0},{"prop":"cafe_table","x":426,"y":271,"rotation":0},{"prop":"cafe_table","x":481,"y":278,"rotation":0},{"prop":"target","x":467,"y":178,"rotation":113},{"prop":"tow","x":677,"y":455,"rotation":0}]}
     ,1: {"background":"bg/plain","props":[{"prop":"tow","x":194,"y":416,"rotation":0},{"prop":"target","x":478,"y":91,"rotation":85},{"prop":"wall","x":298,"y":94,"rotation":0},{"prop":"wall","x":606,"y":88,"rotation":0}]}
 };
